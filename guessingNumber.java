0ad4b63e181a2dc96357528fbaa0a128d834a54b
Hsiang-ting <s1093524@mail.yzu.edu.tw>
2024-04-05 21:11:05 +0800

    M: Generate non-duplicate numbers

--- guessingNumber.java
+++ guessingNumber.java
@@ -2,10 +2,28 @@ public class guessingNumber {
     static int genRandNum() {
         return (int)(Math.random()*10);
     }
+    static void printSecret(int[] secret) {
+        for(int i=0; i < secret.length; ++i)
+            System.out.print(secret[i]);
+        System.out.println();
+    }
     public static void main(String[] args) {
         int secret[] = new int[4];
 
         // generate number from 1-9
-        System.out.println(genRandNum());
+        secret[0] = genRandNum();
+        secret[1] = genRandNum();
+        secret[2] = genRandNum();
+        secret[3] = genRandNum();
+
+        // avoid a duplcate number
+        while(secret[1]==secret[0]) 
+            secret[1] = genRandNum();
+        while(secret[2]==secret[1] || secret[2]==secret[0])
+            secret[2] = genRandNum();
+        while(secret[3]==secret[2] || secret[3]==secret[1] || secret[3]==secret[0])
+            secret[3] = genRandNum();
+
+        printSecret(secret);
     }
 }
