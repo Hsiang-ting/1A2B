b642507f9cc746a3eb7b3358e413ef2eebfde13b
Hsiang-ting <s1093524@mail.yzu.edu.tw>
2024-04-06 15:12:11 +0800

    F: Finish version 2 (use abstract class)

--- guessNumber.java
+++ guessNumber.java
@@ -6,104 +6,119 @@ public class guessNumber {
 
     static abstract class Number {
         protected int numbers[];
-        Number() { numbers = new int[DIGIT]; }
+        Number() { 
+            numbers = new int[DIGIT]; 
+        }
+
         abstract void genNumbers();
         
         final int[] getNumbers() {
             return numbers;
         }
 
-        void printNumber(final int[] number) {
-            for(int i=0; i < number.length; ++i)
-                System.out.print(number[i]);
+        void printNumber() {
+            for(int i=0; i < DIGIT; ++i)
+                System.out.print(numbers[i]);
             System.out.println();
         }
-    }
 
+        void getAB(Number num, int[] AB) {
+            // initialize AB
+            AB[0]=AB[1]=0;
+            int nums[] = num.getNumbers();
+            
+            // record appeard numbers without the number in the right position
+            int appear[] = new int[VALID_NUM];
+            for(int i=0; i < DIGIT; ++i) {
+                if(numbers[i] == nums[i]) AB[0]++;
+                else {
+                    if(appear[numbers[i]] > 0) AB[1]++;
+                    else appear[numbers[i]]++;
 
-
-    static int genRandNum() {
-        return (int)(Math.random()*VALID_NUM);
-    }
-
-    static boolean isDuplicate(final int[] guess) {
-        int appear[] = new int[VALID_NUM];
-        for(int i=0; i < guess.length; ++i)
-            if(appear[guess[i]] > 0) return true;
-            else appear[guess[i]]++;
-        return false;
-    }
-
-    
-    static boolean isCorrect(final int[] secret, final int[] guess, int[] AB) {
-        AB = new int[2];
-        // record appeard numbers without the number in the right position
-        int appear[] = new int[VALID_NUM];
-        for(int i=0; i < secret.length; ++i) {
-            if(secret[i] == guess[i]) AB[0]++;
-            else {
-                if(appear[guess[i]] > 0) AB[1]++;
-                else appear[guess[i]]++;
-
-                if(appear[secret[i]] > 0) AB[1]++;
-                else appear[secret[i]]++;
+                    if(appear[nums[i]] > 0) AB[1]++;
+                    else appear[nums[i]]++;
+                }
             }
         }
-        return AB[0]==4? true:false;
     }
 
-    public static void main(String[] args) {
-        int secret[] = new int[4];
-        int guess[] = new int[4];
+    static class Secret extends Number {
+        Secret() {
+            genNumbers();
+            printNumber();
+        }
+        void genNumbers() {
+            // generate number from 1-9
+            numbers[0] = genRandNum();
+            numbers[1] = genRandNum();
+            numbers[2] = genRandNum();
+            numbers[3] = genRandNum();
 
-        // AB[0] for a correct number in the correct position
-        // AB[1] for a correct number in a wrong position
-        int AB[] = new int[2];
+            // avoid a duplcate number
+            while(numbers[1]==numbers[0]) 
+                numbers[1] = genRandNum();
+            while(numbers[2]==numbers[1] || numbers[2]==numbers[0])
+                numbers[2] = genRandNum();
+            while(numbers[3]==numbers[2] || numbers[3]==numbers[1] || numbers[3]==numbers[0])
+                numbers[3] = genRandNum();
+        }
+        
+        private int genRandNum() {
+            return (int)(Math.random()*VALID_NUM);
+        }   
+    }
 
-        // generate number from 1-9
-        secret[0] = genRandNum();
-        secret[1] = genRandNum();
-        secret[2] = genRandNum();
-        secret[3] = genRandNum();
-
-        // avoid a duplcate number
-        while(secret[1]==secret[0]) 
-            secret[1] = genRandNum();
-        while(secret[2]==secret[1] || secret[2]==secret[0])
-            secret[2] = genRandNum();
-        while(secret[3]==secret[2] || secret[3]==secret[1] || secret[3]==secret[0])
-            secret[3] = genRandNum();
-
+    static class Guess extends Number {
         Scanner input = new Scanner(System.in);
-        do {
-            // initialize guess
-            for(int i=0; i < guess.length; ++i) guess[i] = 0;
 
-            // if not valid input
-            while(isDuplicate(guess)) {
+        void genNumbers() {    
+            while(true) {
                 System.out.print("Guess a number: ");
                 int number = input.nextInt();
-                
-                // check if the number is valid
-                if(number < 0 || number > 9999) {
-                    System.out.println("Invalid Input");
-                    continue;
-                }
+                    
                 // split a four-digit number into four numbers
-                guess[0] = number/1000;
+                numbers[0] = number/1000;
                 number%=1000;
-                guess[1] = number/100;
+                numbers[1] = number/100;
                 number%=100;
-                guess[2] = number/10;
+                numbers[2] = number/10;
                 number%=10;
-                guess[3] = number;
-            }
-            
-            if(AB[0]>0) System.out.print(AB[0]+'A');
-            if(AB[1]>0) System.out.println(AB[1]+'B');
+                numbers[3] = number;
 
-        }while(!isCorrect(guess, secret, AB));
+                if(isValid()) return;
+                else 
+                    System.out.println("Invalid input!");
+            }
+        }
+        private boolean isValid() {
+            int appear[] = new int[VALID_NUM];
+            for(int i=0; i < DIGIT; ++i) {
+                if(numbers[i] < 0 || numbers[i] > 9) return false;
+                if(appear[numbers[i]] > 0) return false;
+                else appear[numbers[i]]++;
+            }
+            return true;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Number secret = new Secret();
+        Number guess = new Guess();
         
-        
+        int AB[] = new int[2];
+
+        while(true) {
+            guess.genNumbers();
+            guess.getAB(secret, AB);
+
+            if(AB[0]>0) System.out.print(AB[0]+"A");
+            if(AB[1]>0) System.out.println(AB[1]+"B");
+            else System.out.println();
+
+            if(AB[0] == DIGIT)
+                break;
+
+        }
     }
 }
